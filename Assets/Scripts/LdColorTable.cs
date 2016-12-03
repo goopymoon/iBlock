e9f5809dfb7ad50b52eed367950fce09446ad7c8
goopymoon <goopymoon@hanmail.net>
2016-12-03 23:05:17 +0900

    Move project personal to iblock group

---
+++ Assets/Scripts/LdColorTable.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+using System;
+
+public class LdColorTable : MonoBehaviour
+{
+    private LdColorLoader _colorLoader;
+    private Dictionary<int, Color32> _palette;
+
+    public Color32 GetColor(int colorIndex)
+    {
+        if (_palette.ContainsKey(colorIndex))
+            return _palette[colorIndex];
+        else
+            return _colorLoader.DefBrickColor;
+    }
+
+    void Awake()
+    {
+        _colorLoader = new LdColorLoader();
+        _palette = new Dictionary<int, Color32>();
+
+        string fileName = _colorLoader.COLOR_CFG_FNAME;
+        if (!_colorLoader.Load(fileName, ref _palette))
+        {
+            Console.WriteLine("Cannot parse: {0}", fileName);
+        }
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/LdModelMesh.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+using System;
+
+public class LdModelMesh : MonoBehaviour {
+
+    public void SetParent(Transform parent)
+    {
+        transform.SetParent(parent, false);
+    }
+
+    public void CreateMesh(List<Vector3> vertices, List<int> triangles, List<Color32> colors)
+    {
+        Mesh mesh = new Mesh();
+
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = triangles.ToArray();
+        mesh.colors32 = colors.ToArray();
+
+        mesh.RecalculateNormals();
+        mesh.RecalculateBounds();
+        mesh.Optimize();
+
+        GetComponent<MeshFilter>().mesh = mesh;
+    }
+
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
